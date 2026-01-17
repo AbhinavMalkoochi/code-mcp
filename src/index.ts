cc88a7408bbb86b9e34b407222d3cb92baeab4bf
Abhinav Malkoochi <Abhinav.malkoochi@gmail.com>
2026-01-16 21:16:38 -0600

    Export SkillGenerator from package

--- src/index.ts
+++ src/index.ts
@@ -14,6 +14,8 @@ export type {
 } from "./discovery/toolDiscovery.js";
 export { TypeGenerator } from "./generator/typeGenerator.js";
 export { CodeGenerator } from "./generator/codeGenerator.js";
+export { SkillGenerator } from "./generator/skillGenerator.js";
+export type { SkillGenerationOptions } from "./generator/skillGenerator.js";
 export { ConfigWatcher } from "./watcher/configWatcher.js";
 export type { WatcherOptions } from "./watcher/configWatcher.js";
 export { ConfigError, ConnectionError, GenerationError } from "./errors.js";
